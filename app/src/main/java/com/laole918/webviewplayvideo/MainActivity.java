6386a55cf29d298fce3e48ad3cdde9155aae2981
laole918 <laole918@qq.com>
2016-04-12 19:03:32 +0800

    初始化

---
+++ app/src/main/java/com/laole918/webviewplayvideo/MainActivity.java
@@ -0,0 +1,132 @@
+package com.laole918.webviewplayvideo;
+
+import android.content.pm.ActivityInfo;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+
+import com.laole918.webviewplayvideo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout mFrameLayout;
+    private View mCustomView = null;
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+        ActivityMainBinding mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mFrameLayout = mBinding.mFrameLayout;
+        mWebView = mBinding.mWebView;
+        initWebView();
+//        mWebView.loadUrl("http://www.bilibili.com/video/av4332894/");
+        mWebView.loadUrl("http://www.iqiyi.com/v_19rrkzntgg.html");
+    }
+
+    private void initWebView() {
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setPluginState(WebSettings.PluginState.ON);
+//        settings.setPluginsEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setLoadWithOverviewMode(true);
+
+//        settings.setJavaScriptEnabled(true);
+		settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setSupportZoom(true);
+        settings.setBuiltInZoomControls(true);
+//		webView.setInitialScale(100);
+        mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        mWebView.setHorizontalScrollBarEnabled(false);
+        mWebView.setVerticalScrollBarEnabled(false);
+        mWebView.setWebChromeClient(new MyWebChromeClient());
+        mWebView.setWebViewClient(new MyWebViewClient());
+    }
+
+    @Override
+    public void onPause() {// 继承自Activity
+        super.onPause();
+        mWebView.onPause();
+    }
+
+    @Override
+    public void onResume() {// 继承自Activity
+        super.onResume();
+        mWebView.onResume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onDestroy() {// 继承自Activity
+        mWebView.destroy();
+        super.onDestroy();
+    }
+
+    class MyWebChromeClient extends WebChromeClient {
+
+        private CustomViewCallback mCustomViewCallback;
+//        private int mOriginalOrientation = 1;
+
+        @Override
+        public void onShowCustomView(View view, WebChromeClient.CustomViewCallback callback) {
+            if (mCustomView != null) {
+                callback.onCustomViewHidden();
+                return;
+            }
+            mFrameLayout.addView(view);
+            mCustomView = view;
+            mCustomViewCallback = callback;
+//            mOriginalOrientation = getRequestedOrientation();
+            mWebView.setVisibility(View.GONE);
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//            super.onShowCustomView(view, callback);
+        }
+
+        public void onHideCustomView() {
+            mWebView.setVisibility(View.VISIBLE);
+            if (mCustomView == null) {
+                return;
+            }
+            mCustomView.setVisibility(View.GONE);
+            mFrameLayout.removeView(mCustomView);
+            mCustomView = null;
+            mCustomViewCallback.onCustomViewHidden();
+            // Show the content view.
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+
+    }
+
+    class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            view.loadUrl(url);
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+    }
+}
